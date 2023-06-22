d2ceb2a98f2cfd7320bc750b8bc69c9d165166a0
kayku8 <reconquistadevteam@gmail.com>
2023-06-22 10:15:29 +0200

    transcribe_audio.py -> delete file always

--- transcribe_audio.py
+++ transcribe_audio.py
@@ -8,19 +8,22 @@ file_name = "yt_video_sound.mp4"
 def transcribe_video(video_url):
     global yt
     try:
-        yt = YouTube(video_url)
-    except:
-        print("Connection error")
+        try:
+            yt = YouTube(video_url)
+        except:
+            print("Connection error")
 
-    yt.streams.filter(file_extension="mp4")
-    stream = yt.streams.get_by_itag(139)
-    stream.download('', file_name)
-    model = whisper.load_model("base")
-    result = model.transcribe(file_name)
+        yt.streams.filter(file_extension="mp4")
+        stream = yt.streams.get_by_itag(139)
+        stream.download('', file_name)
+        model = whisper.load_model("base")
+        result = model.transcribe(file_name)
+        deleteFile()
+        return result["text"]
+    except:
+        deleteFile()
+
+
+def deleteFile():
     if os.path.exists(file_name):
         os.remove(file_name)
-    return result["text"]
-
-
-if __name__ == "__main__":
-    print(transcribe_video("https://www.youtube.com/shorts/bh4qOXep4ck"))
