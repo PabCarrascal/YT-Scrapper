234528e9d95e6d08189882ad80267f0b5cf245e1
kayku8 <reconquistadevteam@gmail.com>
2023-12-05 12:50:41 +0100

    deleted youtube client

--- globals.py
+++ globals.py
@@ -1,10 +1,4 @@
 mongo_client = None
-youtube_client = None
-
-
-def set_global_youtube_client(youtube):
-    global youtube_client
-    youtube_client = youtube
 
 
 def set_global_mongo_client(mongo):
